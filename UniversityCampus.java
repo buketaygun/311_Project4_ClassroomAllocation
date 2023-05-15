da072156d1c39afa93b6ddbd8457beb37ba04a38
zeynepoykuerdem <81776509+zeynepoykuerdem@users.noreply.github.com>
2023-05-15 23:09:33 +0300

    Delete UniversityCampus.java

--- UniversityCampus.java
+++
@@ -1,8 +0,0 @@
-public interface UniversityCampus { //component
-
-    void Add(UniversityCampus universityCampus);
-    void Remove(UniversityCampus universityCampus);
-    void Display(int idx); //kullanılmazsa sil
-    public String getName();
-
-}
